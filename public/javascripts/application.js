2c43af70f459192c888c725991e5913653e71f56
spearwolf <wolfger@spearwolf.de>
2008-06-06 15:53:30 +0200

    minor code refactoring

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,50 +1,59 @@
 
-Ext.onReady(function(){
+Ext.namespace('burndown.forms');
 
-  /*
-   * ================  Iteration form  =======================
-   */
+
+burndown.forms.CreateIterationForm = function(){ return new Ext.FormPanel({
+
+  labelWidth:   75, // label settings here cascade unless overridden
+  url:          'save-form.php',
+  frame:        true,
+  title:        'Create An Iteration',
+  bodyStyle:    'padding:5px 5px 0',
+  width:        350,
+  defaultType:  'textfield',
+
+  defaults: {
+    width: 230
+  },
+
+  items: [{
+      fieldLabel: 'Name',
+      name: 'name',
+      allowBlank:false
+    },{
+      fieldLabel: 'Description',
+      name: 'description'
+    },{
+      fieldLabel: 'Company',
+      name: 'company'
+    },{
+      fieldLabel: 'Email',
+      name: 'email',
+      vtype:'email'
+    }, new Ext.form.TimeField({
+      fieldLabel: 'Time',
+      name: 'time',
+      minValue: '8:00am',
+      maxValue: '6:00pm'
+    })
+  ],
+
+  buttons: [{
+      text: 'Save'
+    }, {
+      text: 'Cancel'
+    }
+  ]
+})}
+
+
+
+
+Ext.onReady(function(){
 
   // Ext.getBody().createChild({tag: 'h2', html: 'Create An Iteration'});
 
-  var iterationForm = new Ext.FormPanel({
-      labelWidth: 75, // label settings here cascade unless overridden
-      url:'save-form.php',
-      frame:true,
-      title: 'Create An Iteration',
-      bodyStyle:'padding:5px 5px 0',
-      width: 350,
-      defaults: {width: 230},
-      defaultType: 'textfield',
-
-      items: [{
-              fieldLabel: 'Name',
-              name: 'name',
-              allowBlank:false
-          },{
-              fieldLabel: 'Description',
-              name: 'description'
-          },{
-              fieldLabel: 'Company',
-              name: 'company'
-          }, {
-              fieldLabel: 'Email',
-              name: 'email',
-              vtype:'email'
-          }, new Ext.form.TimeField({
-              fieldLabel: 'Time',
-              name: 'time',
-              minValue: '8:00am',
-              maxValue: '6:00pm'
-          })
-      ],
-
-      buttons: [{
-          text: 'Save'
-      },{
-          text: 'Cancel'
-      }]
-  });
+  var iterationForm = burndown.forms.CreateIterationForm();
 
   iterationForm.render(document.body);
 })
