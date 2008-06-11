d81834338ab06af8611f874295430285f83e30c1
sleistner <sleistner@gmail.com>
2008-06-11 17:02:15 +0200

    canvas based chart impl. as a concept

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,59 +1,111 @@
 
-Ext.namespace('burndown.forms');
+// Namespace.create('burndown');
 
+function draw(data) {
+  var canvas = $('canvas');
+  var ctx = canvas.getContext('2d');
+  CanvasTextFunctions.enable(ctx);
 
-burndown.forms.CreateIterationForm = function(){ return new Ext.FormPanel({
+  // var dimensions = document.viewport.getDimensions();
+  canvas.width =  580;//dimensions.width;
+  canvas.height = 360;//dimensions.height;
 
-  labelWidth:   75, // label settings here cascade unless overridden
-  url:          'save-form.php',
-  frame:        true,
-  title:        'Create An Iteration',
-  bodyStyle:    'padding:5px 5px 0',
-  width:        350,
-  defaultType:  'textfield',
+  var gap = 25;
+  var width = canvas.getWidth() - gap;
+  var height = canvas.getHeight() -gap;
+  
+  var maxX = data.days.size();
+  var stepX = width / maxX;
+  
+  var rated_days = data.days.reject(function(e) { return e.left == null });
+  var max_hours = rated_days.map(function(e){return e.left}).toArray().sort(function(a,b) {return a - b}).last() + 10;
+  var maxY = Math.max(data.hours_estimated, max_hours);
+  var stepY = 10;
+  var stepsY = maxY / stepY;
+  var wideY = (height / stepsY);
+  
+  var font = "sans";
+  var fontsize = 6;
 
-  defaults: {
-    width: 230
-  },
+  var valueY = function(p) {
+    return height - p * (height / maxY)
+  };
+  
+  var valueX = function(day) {
+    var entry = data.days.detect(function(e) { return e.day == day });
+    return gap + (data.days.indexOf(entry) * stepX);
+  };
+  
+  // axes
+  ctx.beginPath();
+  ctx.lineWidth = .5;
+  ctx.moveTo(gap, 0);
+  ctx.lineTo(gap, height);
+  ctx.lineTo(gap + width, height);
+  ctx.stroke();
+  ctx.save();
 
-  items: [{
-      fieldLabel: 'Name',
-      name: 'name',
-      allowBlank:false
-    },{
-      fieldLabel: 'Description',
-      name: 'description'
-    },{
-      fieldLabel: 'Company',
-      name: 'company'
-    },{
-      fieldLabel: 'Email',
-      name: 'email',
-      vtype:'email'
-    }, new Ext.form.TimeField({
-      fieldLabel: 'Time',
-      name: 'time',
-      minValue: '8:00am',
-      maxValue: '6:00pm'
-    })
+  // x
+  ctx.lineWidth = .06;
+  for(var i = 0; i < maxX; i++) {
+    var x = gap + (i * stepX);
+    var label = data.days[i].day.toString();
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, height + (gap / 2));
+    ctx.stroke();
+    ctx.drawText(font, fontsize, x - (fontsize / 2), height + (gap / (i % 2 == 0 ? 2 : 1)), label);
+    ctx.save();
+  }
+
+  for(var i = stepsY; i >= 0; i--) {
+    var y = i * wideY;
+    ctx.beginPath();
+    ctx.moveTo(gap / 2, y);
+    ctx.lineTo(gap + width, y);
+    ctx.stroke();
+    ctx.drawText(font, fontsize, 0, y - (fontsize / 2), (((i - stepsY) * - 1) * stepY) + '');
+    ctx.save();
+  }
+
+  // optimal line
+  ctx.beginPath();
+  ctx.lineWidth = .5;
+  ctx.moveTo(valueX(data.days.first().day), valueY(data.hours_estimated));
+  ctx.lineTo(valueX(data.days.last().day), valueY(0));
+  ctx.stroke();
+  ctx.save();
+  
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = "green";
+  var prev = data.days.first();
+  rated_days.each(function(e) {
+    ctx.beginPath();
+    ctx.moveTo(valueX(prev.day), valueY(prev.left));
+    ctx.lineTo(valueX(e.day), valueY(e.left));
+    ctx.stroke();
+    ctx.save();
+    prev = e;
+  });
+}
+
+var data = {
+  days: [
+    { day: 1 , left: 150 },
+    { day: 2 , left: 170 },
+    { day: 3 , left: 168 },
+    { day: 4 , left: 140 },
+    { day: 5 , left: 155 },
+    { day: 6 , left: 120 },
+    { day: 7 , left: 100 },
+    { day: 8 , left: 80 },
+    { day: 9 , left: 30 },
+    { day: 10, left: null },
+    { day: 11, left: null },
+    { day: 14, left: null },
+    { day: 15, left: null }
   ],
+  hours_estimated: 150
+};
 
-  buttons: [{
-      text: 'Save'
-    }, {
-      text: 'Cancel'
-    }
-  ]
-})}
-
-
-
-
-Ext.onReady(function(){
-
-  // Ext.getBody().createChild({tag: 'h2', html: 'Create An Iteration'});
-
-  var iterationForm = burndown.forms.CreateIterationForm();
-
-  iterationForm.render(document.body);
-})
+document.observe('dom:loaded', draw.bind(this, data));
