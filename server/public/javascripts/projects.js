11c6037c62fb95395d870fbdf9e06944c485cc60
spearwolf <wolfger@spearwolf.de>
2008-06-13 18:10:52 +0200

    gui styling

--- server/public/javascripts/projects.js
+++ server/public/javascripts/projects.js
@@ -1,32 +1,45 @@
-Base = new Class({
+
+Resource = new Class({
 
   configure: function(config) {
     this.element = $(config.type);
     this.type    = config.type;
     this.root    = config.root;
   },
-  
+
   load: function(url) {
-    new Request.JSON({ url: url || '/' + this.type, method: 'get', onComplete: this.render.bind(this) }).send();
+    new Request.JSON({url: url || '/' + this.type, method: 'get', onComplete: this.onLoadComplete.bind(this) }).send();
   },
-  
+
+  onLoadComplete: function(items) {
+    this.render(items.map(function(it) { return it[this.root] }, this));
+  },
+
   render: function(items) {
-    var select = new Element('select', { size: 7 }), root = this.root;
-    select.addEvent('change', this.onChanged.bind(this));
-    
-    this.element.empty().adopt(select.adopt(items.map(function(e) { return e[root] }).map(function(item) {
-      return new Element('option', { value: item.id }).appendText(item.name);
+    this.element.empty().adopt( this.createContainerElement().adopt( items.map( function(it) {
+      return this.createItemElement(it);
     }, this)));
   },
-  
+
+  createContainerElement: function() {
+    var container = new Element('select', { size: 7 });
+    container.addEvent('change', this.onChanged.bind(this));
+    return container;
+  },
+
+  createItemElement: function(it) {
+    return new Element('option', { value: it.id }).appendText(it.name);
+  },
+
   onChanged: function(event) {
     document.fireEvent(this.root + ':changed', event.target.getSelected().get('value'));
   }
-  
+
 });
 
+
 Projects = new Class({
-  Extends: Base,
+  Implements: Resource,
   
   initialize: function() {
     this.configure({ type: 'projects', root: 'project' });
@@ -34,36 +47,38 @@ Projects = new Class({
   }
 });
 
+
 Iterations = new Class({
-  Extends: Base,
+  Implements: Resource,
 
   initialize: function() {
     this.configure({ type: 'iterations', root: 'iteration' });
     document.addEvent('project:changed', this.onProjectChanged.bind(this));
   },
-  
+
   onProjectChanged: function(project_id) {
     this.load('/projects/' + project_id + '/' + this.type);
   }
 });
 
+
 Stories = new Class({
-  Extends: Base,
+  Implements: Resource,
 
   initialize: function() {
     this.configure({ type: 'stories', root: 'story' });
     document.addEvent('iteration:changed', this.onIterationChanged.bind(this));
   },
-  
+
   onIterationChanged: function(iteration_id) {
     this.load('/iterations/' + iteration_id + '/' + this.type);
   }
 });
 
+
+
 window.addEvent('load', function() {
-  projects = new Projects();
+  projects   = new Projects();
   iterations = new Iterations();
-  stories = new Stories();
+  stories    = new Stories();
 });
-
-
