d9e2ab99148d36b5c1c08104401e8f62779acc6c
sleistner <sleistner@gmail.com>
2008-07-08 18:35:45 +0200

    format

--- server/config/initializers/asset_tag_helper.rb
+++ server/config/initializers/asset_tag_helper.rb
@@ -1,1 +1,2 @@
-ActionView::Helpers::AssetTagHelper::register_javascript_include_default 'mootools-core', 'mootools-more', 'jquery-1.2.6.min', 'jquery-ui-datepicker-1.5.1.min', 'chart'
+ActionView::Helpers::AssetTagHelper::register_javascript_include_default  'mootools-core', 'mootools-more', 
+                                                                          'jquery-1.2.6.min', 'jquery-ui-datepicker-1.5.1.min', 'chart'
--- server/vendor/plugins/acts_as_resource_controller/lib/acts_as_resource_controller.rb
+++ server/vendor/plugins/acts_as_resource_controller/lib/acts_as_resource_controller.rb
@@ -33,16 +33,6 @@ module ActsAsResourceController
 
   module ResourceMethods
 
-    def create
-      m = model.new params[model_name]
-      m.send("#{belongs_to_id}=", params[belongs_to_id]) if belongs_to?
-      self.instance = m
-      m.save!
-      headers['Location'] = send "#{model_name}_url", instance
-      send(after_create) unless after_create.nil?
-      head :created
-    end
-
     def index
       self.instances = with_scope do
         options = returning(:order => order) do |o|
@@ -57,6 +47,20 @@ module ActsAsResourceController
     def show
       self.instance = with_scope { model.find params[:id] }
       render_formats instance
+    end
+
+    def create
+      m = model.new params[model_name]
+      m.send("#{belongs_to_id}=", params[belongs_to_id]) if belongs_to?
+      self.instance = m
+      m.save!
+      headers['Location'] = send "#{model_name}_url", instance
+      send(after_create) unless after_create.nil?
+      head :created
+    end
+    
+    def edit
+      self.instance = model.find params[:id]
     end
 
     def update
@@ -113,7 +117,8 @@ module ActsAsResourceController
 
     def extend_to_format obj
       obj.instance_variable_set "@format_options", format_options
-
+      
+      # TODO: refactore the lines below to an simple :format for loop
       obj.class.send(:alias_method, :to_json_orig, :to_json)
       def obj.to_json options = {}
         to_json_orig options.merge(@format_options)
@@ -152,7 +157,7 @@ module ActsAsResourceController
     end
 
     def errors_as_hash errors
-      returning({}) {|h| errors.each {|attr, msg| h[attr] = msg.gsub(/%\{/, '#{') }}
+      returning({}) { |h| errors.each { |attr, msg| h[attr] = msg.gsub(/%\{/, '#{') } }
     end
 
     def render_record_not_found exception
